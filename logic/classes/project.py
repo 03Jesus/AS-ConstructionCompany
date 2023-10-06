6f2100a7eadcaee6455565057e56e98821ea7ed0
03Jesus <saavedraj@utb.edu.co>
2023-10-06 17:12:49 -0500

    rolling back

--- logic/classes/project.py
+++ logic/classes/project.py
@@ -236,8 +236,3 @@ if __name__ == "__main__":
     
     print(project.__str__())
 
-    print(project == project)
-    print(project == project)
-    print(project == project)
-    print(project == project)
-
